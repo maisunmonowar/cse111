cd4650abcf5a90b806dcb35513b11d687c3ea084
cb3ss <cb3ss@cb3ss-PC>
2015-08-09 20:33:59 +0600

    initial upload

---
+++ src/hospitalmanagement/accounts.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalmanagement;
+
+/**
+ *
+ * @author cb3ss
+ */
+public class accounts {
+    public static double cash = 10000;//starting capital
+   
+}
